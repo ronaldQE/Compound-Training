e5de9cb83efc6d969e47d7b0fd3a3ca846af65f3
Ezequiel Rene Lafuente Machado <ezqui016@gmail.com>
2021-05-11 19:51:00 -0400

    Creación vista Register User

---
+++ screens/UserDetail.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text} from 'react-native'
+const UserDetail = () =>{
+    return (
+        <View>
+            <Text>Users List </Text>
+        </View>
+    )
+}
+export default UserDetail
---
+++ screens/UserList.js
@@ -0,0 +1,10 @@
+import React  from 'react'
+import { View, Text} from 'react-native'
+const UserList = () =>{
+    return (
+        <View>
+            <Text>Users List </Text>
+        </View>
+    )
+}
+export default UserList
